6ea35c45406c0be46bcd0e37fb2542f91c1e1c70
Carlos Revilla <clro26.k@gmail.com>
2020-03-12 21:22:40 +0100

    main page with popular movies using javascript

--- cine.js
+++ cine.js
@@ -2,6 +2,22 @@ const searchInput = document.querySelector('.searchInput');
 const peliculasContainer = document.querySelector('.peliculas');
 const searchForm = document.querySelector('.search-container')
 
+{axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&page=1')
+        .then(res => {
+            const peliculas = res.data.results
+            peliculasContainer.innerHTML = '';
+            const baseImgUrl = 'https://image.tmdb.org/t/p/w185';
+            peliculas.forEach(pelicula => {
+                const imagen = pelicula.poster_path ? `
+        <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : ''
+            peliculasContainer.innerHTML += `
+            <div class="peliculas-descripcion"><h2>${pelicula.title}</h2>
+            ${imagen}</div>
+            `
+            });
+        })
+}
+
 const searchPeliculas = (busqueda) => {
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query=' + busqueda)
         .then(res => {
